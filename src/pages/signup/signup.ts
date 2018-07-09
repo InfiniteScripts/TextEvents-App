9460214949d8662c64d478f3bd4a429e7febf481
Kevin Greene <kevin@infinitescripts.com>
2018-07-08 19:12:34 -0500

    404 api error resolved

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { IonicPage, NavController, ToastController } from 'ionic-angular';
-import { Pro } from '@ionic/pro';
+
 import { User } from '../../providers/providers';
 import { MainPage } from '../pages';
 
@@ -52,12 +52,11 @@ export class SignupPage {
       console.error(resp);
     }, (err) => {
 
-      Pro.monitoring.log(err,  { level: 'error' });
       //this.navCtrl.push(MainPage);
 
       // Unable to sign up
       let toast = this.toastCtrl.create({
-        message: err.toString(),
+        message: this.signupErrorString,
         duration: 5000,
         position: 'top'
       });
